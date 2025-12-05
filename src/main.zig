f79393c87db0e128cbb1b49a25e40a043f1a817e
BradenEverson <bradeneverson@gmail.com>
2025-12-05 17:50:20 -0600

    Read out the file in main

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,25 @@
 const std = @import("std");
 
 pub fn main() void {
+    // const alloc = std.heap.page_allocator;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const alloc = gpa.allocator();
+
     var args = std.process.args();
     _ = args.skip();
 
-    if (args.next()) |file| {
-        std.debug.print("Compiling {s}\n", .{file});
+    if (args.next()) |file_path| {
+        const data = std.fs.cwd().readFileAlloc(alloc, file_path, 65536) catch {
+            std.debug.print("Error: File does not exist!\n", .{});
+            std.process.exit(1);
+        };
+
+        defer alloc.free(data);
+
+        std.debug.print("{s}\n", .{data});
     } else {
         std.debug.print("Missing Input file!!!\nUsage: ./redstone 'file.vhdl' or whatever\n", .{});
     }
