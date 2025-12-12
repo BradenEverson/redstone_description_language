43787bd4e7f3968e87f0c397e0c27b774c2a6a24
BradenEverson <bradeneverson@gmail.com>
2025-12-12 11:42:13 -0600

    Create full stubbed out pipeline

--- src/construction.zig
+++ src/construction.zig
@@ -1,17 +1,21 @@
 //! Intermediate Representation for building the physical Minecraft entity
 
+const std = @import("std");
+
 const Circuit = @import("dl.zig").Circuit;
 const NbtNode = @import("nbt/node.zig").NbtNode;
 
 pub const CircuitEntity = struct {
-    pub fn toNbt(self: *CircuitEntity) NbtNode {
+    pub fn toNbt(self: *const CircuitEntity, alloc: std.mem.Allocator) !NbtNode {
         _ = self;
+        _ = alloc;
 
         return .{ .name = null, .ty = .end };
     }
 };
 
-pub fn translateToEntity(circuit: Circuit) CircuitEntity {
+pub fn translateToEntity(alloc: std.mem.Allocator, circuit: Circuit) !CircuitEntity {
+    _ = alloc;
     _ = circuit;
     return .{};
 }
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
 
 const dl = @import("dl.zig");
+const construction = @import("construction.zig");
 const vhdl = @import("vhdl.zig");
-const world = @import("nbt.zig");
+const nbt = @import("nbt.zig");
 
 pub fn main() void {
     // const alloc = std.heap.page_allocator;
@@ -16,7 +17,6 @@ pub fn main() void {
     defer arena.deinit();
 
     const nbt_arena = arena.allocator();
-    _ = nbt_arena;
 
     var args = std.process.args();
     _ = args.skip();
@@ -33,7 +33,14 @@ pub fn main() void {
             std.debug.print("Failed to Parse VHDL file\n", .{});
             std.process.exit(1);
         };
-        _ = circuit;
+
+        const ir = construction.translateToEntity(alloc, circuit) catch @panic("Failed to translate");
+        const nbt_ir = ir.toNbt(nbt_arena) catch @panic("Failed to create NBT IR");
+
+        var bytes = std.ArrayList(u8){};
+
+        nbt_ir.toBytes(alloc, &bytes, true, true) catch @panic("Failed to translate to bytes");
+        nbt.zipNbt("out.nbt", bytes.items) catch @panic("Failed to zip to NBT");
     } else {
         std.debug.print("Missing Input file!!!\nUsage: ./redstone 'file.vhd' or whatever\n", .{});
     }
