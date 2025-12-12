780792255bb607c546e5353ccb04d0f21084e818
BradenEverson <bradeneverson@gmail.com>
2025-12-12 11:08:03 -0600

    Stub out how the VHDL API will be used

--- src/vhdl.zig
+++ src/vhdl.zig
@@ -1,5 +1,17 @@
 //! Subset of the VHDL Spec that allows describing digial logic systems
 
+const std = @import("std");
+
+const dl = @import("dl.zig");
+const Circuit = dl.Circuit;
+
+pub fn parseToCircuit(alloc: std.mem.Allocator, path: []const u8) !Circuit {
+    _ = alloc;
+    _ = path;
+
+    return error.UnexpectedEOF;
+}
+
 test {
     _ = @import("vhdl/tokenizer.zig");
     _ = @import("vhdl/parser.zig");
