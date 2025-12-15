601d2bde026a157bfb3f29f8887dc214f232f53e
BradenEverson <bradeneverson@gmail.com>
2025-12-15 06:04:02 -0600

    Begin entity representation

--- src/construction.zig
+++ src/construction.zig
@@ -5,7 +5,48 @@ const std = @import("std");
 const Circuit = @import("dl.zig").Circuit;
 const NbtNode = @import("nbt/node.zig").NbtNode;
 
+pub const BlockType = enum(u8) {
+    redstone_dust,
+    redstone_torch,
+    comparator,
+    repeater,
+    air = 0x0,
+};
+
+pub const Block = union(BlockType) {
+    redstone_dust,
+    redstone_torch,
+    comparator: u8,
+    repeater: u8,
+    air,
+};
+
 pub const CircuitEntity = struct {
+    width: usize,
+    height: usize,
+    length: usize,
+
+    blocks: []Block,
+
+    pub fn init(alloc: std.mem.Allocator, w: usize, h: usize, l: usize) !CircuitEntity {
+        const buf = try alloc.alloc(Block, w * h * l);
+        for (buf) |*item| {
+            item = .air;
+        }
+
+        return CircuitEntity{
+            .width = w,
+            .height = h,
+            .length = l,
+
+            .blocks = buf,
+        };
+    }
+
+    pub fn deinit(self: *CircuitEntity, alloc: std.mem.Allocator) void {
+        alloc.free(self.blocks);
+    }
+
     pub fn toNbt(self: *const CircuitEntity, alloc: std.mem.Allocator) !NbtNode {
         _ = self;
         _ = alloc;
