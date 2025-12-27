04f378db37926bbe542d4f979dd4d7e4c7011176
BradenEverson <bradeneverson@gmail.com>
2025-12-27 08:54:29 -0600

    Update example vhdl

--- fa.vhd
+++ fa.vhd
@@ -1,14 +1,8 @@
--- RHDL compiler does not require std_logic imports
--- library ieee;
--- use ieee.std_logic_1164.all;
-
--- block diagram symbol
 entity FA is
 port(	A, B, CIN	: 	in 	std_logic;
 		COUT, S		: 	out 	std_logic);
 end entity FA;
 
--- circuit design
 architecture LOGIC of FA is
 begin
 	COUT <= (A and B) or (A and CIN) or (B and CIN);
