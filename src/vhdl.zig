d0731c4b70210be1427821fe7be04a80b5e77bb1
BradenEverson <bradeneverson@gmail.com>
2025-12-27 10:25:29 -0600

    Parser workflow

--- src/vhdl.zig
+++ src/vhdl.zig
@@ -5,11 +5,34 @@ const std = @import("std");
 const dl = @import("dl.zig");
 const Circuit = dl.Circuit;
 
-pub fn parseToCircuit(alloc: std.mem.Allocator, data: []const u8) !Circuit {
-    _ = alloc;
-    _ = data;
+const tokenizer = @import("vhdl/tokenizer.zig");
+const Token = tokenizer.Token;
 
-    return error.UnexpectedEOF;
+const parse = @import("vhdl/parser.zig");
+const Parser = parse.Parser;
+const TopLevel = parse.TopLevel;
+
+pub fn parseToCircuit(alloc: std.mem.Allocator, data: []const u8) !Circuit {
+    const circuit = Circuit{};
+
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    const a_alloc = arena.allocator();
+
+    var tokens = std.ArrayList(Token){};
+    defer tokens.deinit(a_alloc);
+
+    try tokenizer.tokenize(data, &tokens, a_alloc);
+
+    var parser = Parser.init(tokens.items);
+
+    var al = std.ArrayList(*const TopLevel){};
+    defer al.deinit(a_alloc);
+
+    try parser.parse(a_alloc, &al);
+
+    return circuit;
 }
 
 test {
